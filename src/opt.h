70268fa85ea048fdd26aff38b83466b73048d46a
toni <matzeton@googlemail.com>
2015-12-09 00:05:56 +0100

    default option

--- src/opt.h
+++ src/opt.h
@@ -2,7 +2,7 @@
 #define OPT_H 1
 
 #define OPT(opt_index) config_opts[opt_index]
-#define GETOPT(opt_index) OPT(opt_index).opt
+#define GETOPT(opt_index) (OPT(opt_index).found != 0 ? OPT(opt_index).opt : OPT(opt_index).def)
 #define OPT_USED(opt_index, uvalue) OPT(opt_index).found = uvalue;
 #define d_OPT(opt_index, rvalue) OPT(opt_index).opt.dec = rvalue; OPT_USED(opt_index, 1);
 #define s_OPT(opt_index, rvalue) OPT(opt_index).opt.str = rvalue; OPT_USED(opt_index, 1);
